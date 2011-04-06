b49c20a48d1da3da4d0c563e0f26172f011fc5c4
Jordan Sissel <jls@semicomplete.com>
2011-04-05 18:52:56 -0700

    - need more sed to fix ruby-1.9.2 to work when relocated.

--- package-ruby.sh
+++ package-ruby.sh
@@ -11,23 +11,38 @@ fi
 
 if [ ! -f $rvm_path/scripts/rvm ] ; then
   # install rvm and retry.
+  echo "RVM not found; installing now to $rvm_path"
   install_rvm
   exec bash $0 "$@"
 fi
 
+# Run this in a subshell so the rvm loading doesn't infect our current shell.
 (
   . $rvm_path/scripts/rvm
   rvm use $wantruby || rvm install $wantruby
 )
 
-echo "patching rvm scripts to remove path '$DESTDIR/'"
-find $DESTDIR/$PREFIX/{wrappers,environments} -type f -print0 \
-  | xargs -0n1 sed -ie "s,$DESTDIR/,,g"
-
-rubyinfo="$(. $rvm_path/scripts/rvm; rvm use $wantruby; rvm info | sed -ne '/^  ruby:/,/^ *$/p')"
+rubyinfo="$(. $rvm_path/scripts/rvm; rvm use $wantruby > /dev/null; rvm info | sed -ne '/^  ruby:/,/^ *$/p')"
 ruby="$(echo "$rubyinfo" | awk -F\" '/ interpreter:/ { print $2 }')"
 version="$(echo "$rubyinfo" | awk -F\" '/ version:/ { print $2 }')"
+rubyname="$(. $rvm_path/scripts/rvm; rvm use $wantruby > /dev/null; rvm current)"
+
+echo "rubyinfo: $rubyinfo"
+echo "ruby: $ruby"
+echo "version: $version"
+echo "name: $rubyname"
+
+echo "patching rvm scripts to remove path '$DESTDIR/'"
+find $DESTDIR/$PREFIX/{wrappers,environments} -type f -print0 \
+  | xargs -0n1 sed -i -e "s,$DESTDIR/,,g"
+find $DESTDIR/$PREFIX/rubies/${rubyname} -type f -name '*.rb' -print0 \
+  | xargs -0n1 sed -i -e "s,$DESTDIR/,,g"
+find $DESTDIR/$PREFIX/rubies/${rubyname} -type f -name '*.h' -print0 \
+  | xargs -0n1 sed -i -e "s,$DESTDIR/,,g"
+for i in $DESTDIR/$PREFIX/{rubies/${rubyname}/bin,wrappers/${rubyname}}/{erb,gem,irb,rake,rdoc,ri,testrb} ; do
+  sed -i -e "s,$DESTDIR/,,g" $i
+done
 
 echo "Packaging up $ruby-$version"
-fpm -s dir -t deb -C ./build -n rvm-${ruby} -v ${version}-1 \
-  opt/rvm/{gems,rubies,wrappers,environments,log,src,bin}/${ruby}-${version}
+fpm -s dir -t deb -C ./build -n rvm-${ruby}-${version} -v ${version}-1 \
+  opt/rvm/{gems,rubies,wrappers,environments,log,src,bin}/${rubyname}
